41461851a763a198d7870b78553e763f831d8506
partum <jarringt@uccs.edu>
2021-03-28 16:00:42 -0600

    starting layout

--- index.php
+++ index.php
@@ -8,12 +8,18 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title></title>
-        <meta name="description" content="">
+        <meta name="description" content="a library app">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@400;600&display=swap" rel="stylesheet"> 
     </head>
     <body>
-    <h1>My first PHP page</h1>
+        <div id='container'>
+            <h1>Moriarty Library</h1>
+    <h2>Serving readers since 1994</h2>
+</div>
+    
 
 <?php
 echo "Hello World!";
@@ -22,6 +28,6 @@ echo "Hello World!";
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         
-        <script src="" async defer></script>
+        <script src="script.js" async defer></script>
     </body>
 </html>
