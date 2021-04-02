68e002ae00869e853ee08815f12550c92f3aecc9
partum <jarringt@uccs.edu>
2021-04-02 14:07:21 -0600

    created super basic admin page
    I might improve it later

---
+++ admin-page.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Administrator Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@400;600&display=swap" rel="stylesheet">
+        <style>
+        h1{
+            font-family: "Rosario", sans-serif;
+            font-weight: 600;
+            font-size: 36px;
+        }
+        a {
+            color: #17bebb;
+            text-decoration: none;
+        }
+        a:hover {
+            color: #faa916;
+            cursor: pointer;
+        }
+        button {
+        font-family: 'Rosario';
+        text-transform: uppercase;
+        outline: 0;
+        background: #faa916;
+        width: 100px;
+        border: 0;
+        padding: 15px;
+        color: #ffffff;
+        font-size: 14px;
+        -webkit-transition: all 0.3 ease;
+        transition: all 0.3 ease;
+        cursor: pointer;
+        border-radius: 10px;
+        position: relative;
+        left: 40vw;
+        }
+        button:hover
+        {
+  background: #17bebb;
+}
+        </style>
+    </head>
+    <body style="text-align: center;">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <button>log out</button>
+        <h1>Would you like to make changes to <a href="admin-users.php">users</a> or <a href="admin-books.php">books</a>?</h1>
+        
+    </body>
+</html>
