3b55b20fadd07e83017983b6e62617bdff59fa58
partum <jarringt@uccs.edu>
2021-04-07 13:10:37 -0600

    add books added

---
+++ add-book.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Add a Book</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <?php include("config.php")?>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <form action="add-entry.php">
+            <label for="">Title</label>
+            <input type="text" name='title'>
+            <label for="">Author</label>
+            <input type="text" name='author'>
+            <label for="">ISBN</label>
+            <input type="text" name='isbn' maxlength='9'>
+            <label for="">Copies</label>
+            <input type="text" name='copies' maxlength='2'>
+            <button type="submit">Add</button>
+        </form>
+        
+    </body>
+</html>
+
+<!-- INSERT INTO `books` (`Title`, `Author`, `ISBN`, `Copies`) VALUES ('Persuasion', 'Jane Austin', '872910483', '12'); -->
