fae34d84cd9b436761d076a28080133042f09fc5
partum <jarringt@uccs.edu>
2021-04-21 14:06:12 -0600

    small style updates

--- delete-book.php
+++ delete-book.php
@@ -1,9 +1,19 @@
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Edit a Book</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="\library-app\style.css">
+        <?php include("scripts/config.php")?>
+        <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@400;600&display=swap" rel="stylesheet">
+</head>
 <?php
     include("scripts/config.php");
     $key = $_GET["key"];
     $sql = "DELETE FROM books WHERE ISBN = '$key'";
     if ($link->query($sql) === TRUE) {
-        echo "Record deleted successfully";
+        echo "<p>Record deleted successfully.</p>";
         echo '<a href="/library-app/admin-books.php">back</a>';
         //header("location:admin-books.php");
       } else {
--- edit-book.php
+++ edit-book.php
@@ -7,15 +7,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Add a Book</title>
+        <title>Edit a Book</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <?php 
-            include("scripts/config.php");
-            ob_start();
-        ?>
-        <!-- This page displays the book info as text input and lets the user update the content-->
+        <link rel="stylesheet" href="\library-app\style.css">
+        <?php include("scripts/config.php")?>
+        <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@400;600&display=swap" rel="stylesheet">
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -52,8 +49,8 @@
                 if($edit)
                 {
                     $link->close(); // Close connection
-                    echo 'it worked';
-                    echo '<a href="/library-app/admin-books.php">back</a>';
+                    echo '<p>Update Successful</p>';
+                    echo '<a href="/library-app/admin-books.php">Return to book admin page.</a>';
                     exit;
                 }
                 else
@@ -72,7 +69,7 @@
             <input type="text" name='isbn' maxlength='9' value=<?php echo $isbn ?>>
             <label for="">Copies</label>
             <input type="text" name='copies' maxlength='2' value=<?php echo $copies ?>>
-            <button type="submit" name='update' value='update'>Edit</button>
+            <button type="submit" name='update' value='update' class='small-button'>Edit</button>
         </form>
 
         
